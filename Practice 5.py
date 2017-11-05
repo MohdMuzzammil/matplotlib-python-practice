b2df7090640e57b4f088bc5f857f4ecc19a35c0d
muzzammil <muzzammilmohammed32@gmail.com>
2017-11-05 10:35:48 +0530

    scattering marker and size

--- Practice 5.py
+++ Practice 5.py
@@ -7,7 +7,9 @@ y1 = [random.randrange(1,100) for i in range(0,50)]
 x2 = [random.randrange(1,100) for i in range(0,50)]
 y2 = [random.randrange(1,100) for i in range(0,50)]
 
-plt.scatter(x1,y1,label="points 1")
+#marker is for styles
+#s is for size of marker
+plt.scatter(x1,y1,label="points 1", marker="*",s=10)
 plt.scatter(x2,y2,label="points 2")
 plt.legend()
 
