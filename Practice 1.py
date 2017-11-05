4c9c583ce9bdecf50d7bc6564b9abddc5453adab
muzzammil <muzzammilmohammed32@gmail.com>
2017-11-05 09:38:18 +0530

    label, title, legends

--- Practice 1.py
+++ Practice 1.py
@@ -1,3 +1,7 @@
 import matplotlib.pyplot as plt
+
+#plotting points
 plt.plot([1,2,3],[2,3,4])
+
+#show the plot
 plt.show()
---
+++ Practice 2.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+#points for line 1
+x1 = [1,2,3]
+y1 = [4,3,6]
+
+#points for line 2
+x2 = [1,2,3]
+y2 = [1,7,4]
+
+
+#title, xlabel and ylabel
+plt.title("Practice 2")
+plt.xlabel("xlabel")
+plt.ylabel("ylabel")
+
+#plotting two lines
+plt.plot(x1,y1,label="first line")
+plt.plot(x2,y2,label="second line")
+
+#calling legend
+plt.legend()
+
+plt.show()
